4e0e8703f11777b5feea66d397daca039efd9c8e
kula <zhengwei@igengmei.com>
2020-05-09 19:14:08 +0800

    add meta file

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.routes import demo
+from app.routes import demo, meta
 
 router: APIRouter = APIRouter()
 router.include_router(demo.router, tags=["demo"], prefix="/demo")
+router.include_router(meta.router, tags=['meta'], prefix='/meta')
